1c5f67235d22f749dd8e692b53d466b2a44449cc
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-01 00:48:28 +0600

    Add comment for clarity on printing even numbers
    Added a comment to clarify the purpose of the loop.

--- task 8.c
+++ task 8.c
@@ -4,7 +4,7 @@ int main() {
     int n, i;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-    printf("First %d even numbers are: ", n);
+    printf("First %d even numbers are: ", n);                                                                // Print the First n Even Numbers
     for(i = 2; i <= n * 2; i = i + 2) {
         printf("%d ", i);
     }
