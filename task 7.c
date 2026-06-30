56cec21927678a66e8d9ced91a6dc244fef5482e
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-01 00:46:40 +0600

    Add comment to explain factorial calculation
    Added a comment to clarify the purpose of the loop.

--- task 7.c
+++ task 7.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &n);
      int factorial = 1;
-        for(i = 1; i<= n; i++)
+        for(i = 1; i<= n; i++)                                                                            // Find the Factorial of a Number
         {
             factorial = factorial * i;
         }
