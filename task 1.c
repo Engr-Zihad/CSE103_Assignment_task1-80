c28fe7225118740a6dc6c095860628d8b6172371
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-01 00:34:34 +0600

    Refactor even/odd number check in task 1.c

--- task 1.c
+++ task 1.c
@@ -5,7 +5,7 @@ printf("Enter a number :");
 scanf("%d",&n);
 if (n%2==0)
 {
-    printf("%d is an even number.",n);
+    printf("%d is an even number.",n);                               // Even or Odd Number
 }
 else
 {
