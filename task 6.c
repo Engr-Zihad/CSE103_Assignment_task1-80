8be541143e01961f6d78550d0a9bb0b41fe9cf84
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-01 00:45:51 +0600

    Add comment to explain sum calculation loop
    Added a comment to clarify the purpose of the loop.

--- task 6.c
+++ task 6.c
@@ -5,7 +5,7 @@ int main()
     int n,sum=0;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-    for(int i=1; i<=n; i++)
+    for(int i=1; i<=n; i++)                                                                    // Find the Sum of First n Natural Numbers
     {
         sum = sum + i;
     }
