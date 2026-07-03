7ecf564e1d52d4f6d8e8966c7142a34859c883d4
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-03 14:53:54 +0600

    Fix formatting issues in task 8.c

--- task 8.c
+++ task 8.c
@@ -1,10 +1,10 @@
+ // Print the First n Even Numbers
 #include <stdio.h>
-
 int main() {
     int n, i;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-    printf("First %d even numbers are: ", n);                                                                // Print the First n Even Numbers
+    printf("First %d even numbers are: ", n);                                                              
     for(i = 2; i <= n * 2; i = i + 2) {
         printf("%d ", i);
     }
